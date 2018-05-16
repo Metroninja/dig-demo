f6e3cda0728064d843c491e3dad97bb5836e9c8e
Erik Steinfeld <eriks@attackpattern.com>
2018-05-15 22:11:02 -0700

    Complete initial pass of Product Details page * Images are now displayed in a selectable list * Clicked on Images will open a modal with the full resolution version of the image.

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, Modal, SafeViewArea, StyleSheet, 
+  Text, TouchableHighlight, TouchableOpacity, View } from 'react-native';
 import { connect } from 'react-redux';
 
 import globalStyle from '../../styles';
@@ -23,11 +24,13 @@ export default class Details extends Component {
     const id = props.navigation.state.params && props.navigation.state.params.id;
     this.state = {
       id,
+      modalVisible: false,
     }
   }
 
   render() {
     const product = this.props.products.find(p => p.id === this.state.id);
+    const modalContent = !!this.state.modalContent && this.state.modalContent.replace('http', 'https');
     return (
       <View style={styles.container}>
         <Text style={styles.title}>{product.title}</Text>
@@ -36,7 +39,28 @@ export default class Details extends Component {
         <Text style={styles.text}>{product.description}</Text>
         <Text style={[styles.text, styles.header]}>Specifications:</Text>
         <Text style={[styles.text, styles.specification]}>{product.specification}</Text>
-        
+        <View style={styles.gallery}>
+          {product.images.map((img, index) => (
+            <TouchableOpacity 
+              style={styles.thumbnail} 
+              onPress={() => this.setState({modalContent: img.original})} 
+              key={index}
+            >
+              <Image style={styles.image} source={{uri: img.thumb.replace('http', 'https')}} />
+            </TouchableOpacity>
+          ))}
+        </View>
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={!!modalContent}>
+          <View style={styles.modal}>
+            {!!modalContent && (<Image style={styles.originalImage} source={{uri: modalContent}} />)}
+            <TouchableHighlight style={styles.close} onPress={() => this.setState({modalContent: ''})}>
+              <Text style={styles.closeText}>X</Text>
+            </TouchableHighlight>
+          </View>
+        </Modal>
       </View>
     )
   }
@@ -60,5 +84,37 @@ const styles = StyleSheet.create({
   }, 
   specification: {
     fontStyle: 'italic'
+  },
+  gallery: {
+    marginTop: 24,
+    ...globalStyle.row
+  },
+  thumbnail: {
+    marginRight: 8,
+    ...globalStyle.shadow,
+  },
+  image: {
+    height: 100,
+    width: 100,
+    resizeMode: 'contain',
+  },
+  modal: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.6)'
+  },
+  originalImage: {
+    height: undefined,
+    width: undefined,
+    flex: 1,
+    resizeMode: 'contain'
+  },
+  close: {
+    position: 'absolute',
+    top: 22,
+    right: 24,
+  },
+  closeText: {
+    fontSize: 24,
+    color: '#FFF'
   }
 });
